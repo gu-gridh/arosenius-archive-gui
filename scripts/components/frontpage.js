201ae141ff3d0aadddf57b34f433d1cd4d22b46e
Unknown <traustid@gmail.com>
2018-05-24 08:48:14 +0200

    Added new front page background

--- scripts/components/frontpage.js
+++ scripts/components/frontpage.js
@@ -54,7 +54,7 @@ export default class FrontPage extends React.Component {
 	}
 
 	loadBackgroundImage() {
-		var rand = Math.round(Math.random()*2)+1;
+		var rand = Math.floor(Math.random()*4)+1;
 		var imageUrl = 'img/background/bg-'+rand+'.jpg';
 
 		var image = new Image();
